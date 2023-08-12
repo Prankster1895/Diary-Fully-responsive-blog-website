046d38311182576bf3d85fab6a701da9c45898d8
Dyuti Dasgupta <127132215+Prankster1895@users.noreply.github.com>
2023-08-12 16:18:05 +0530

    Update compose.js

--- compose.js
+++ compose.js
@@ -1,14 +1,20 @@
 const submitButton = document.getElementById("submitButton");
 const blogInput = document.getElementById("blogInput");
+const dateInput = document.getElementById("Date");
+const titleInput = document.getElementById("Title");
 
 submitButton.addEventListener("click", function() {
     console.log("Button Clicked")
     const blogText = blogInput.value.trim();
-    if (blogText !== "") {
+    dateText = dateInput.value.trim();
+    titleText = titleInput.value.trim();
+    if (blogText !== "" && dateText !="" ) {
         // Store the blog text in local storage
         localStorage.setItem("blogText", blogText);
+        localStorage.setItem("dateText", dateText);
+        localStorage.setItem("titleText", titleText);
+
         // Redirect to the display page
         window.location.href = "display.html";
     }
 });
-
